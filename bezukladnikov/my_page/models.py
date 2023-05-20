a6a51579659f4bd89acd5dda3857b32b8f0042a8
iaroslavbezukladnikov <ybezukladnikov@gmail.com>
2023-05-20 21:05:31 +0300

    work with db

--- bezukladnikov/my_page/models.py
+++ bezukladnikov/my_page/models.py
@@ -8,3 +8,6 @@ class Project(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.title
+
