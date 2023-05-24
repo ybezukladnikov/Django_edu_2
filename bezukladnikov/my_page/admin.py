714b0d5fe9158b2b292a732f017030068293d79a
Iaroslav Bezukladnikov <ybezukladnikov@gmail.com>
2023-05-24 15:49:43 +0300

    work with admin panel

--- bezukladnikov/my_page/admin.py
+++ bezukladnikov/my_page/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Category, CategoryAdmin)
--- bezukladnikov/my_page/apps.py
+++ bezukladnikov/my_page/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class MyPageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'my_page'
-    verbose_name = 'Лучшие проекты мои'
+    verbose_name = 'Outdoor sports grounds'
